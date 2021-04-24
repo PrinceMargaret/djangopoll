189cdee79f67b72f18705af6522ab15f5b384350
PrinceMargaret <princeagrahari2000@gmail.com>
2021-04-24 12:20:28 +0530

    add

--- pollster/settings.py
+++ pollster/settings.py
@@ -69,10 +69,15 @@ TEMPLATES = [
         },
     },
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 WSGI_APPLICATION = 'pollster.wsgi.application'
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -121,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
